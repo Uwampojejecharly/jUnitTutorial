0071d0b5b387d6b30e8013052e801747faa4aa01
unknown <uwampojejecharly@gmail.com>
2024-03-08 09:30:38 +0200

    Added new tests and discovered more about tests
    In this commit i discovered more new topics as:
    	- Using some annotations like beforeClass,afterClass,...
    	- Using hamcrest matchers to do assertions.
    	- TDD

--- src/main/java/fr/charly/jUnitTutorial/Calculator.java
+++ src/main/java/fr/charly/jUnitTutorial/Calculator.java
@@ -3,7 +3,26 @@ package fr.charly.jUnitTutorial;
 public class Calculator {
 
 	public Integer add(int i, int i1) {
-		return i+i1;
+		return i + i1;
+	}
+
+	public Integer multiply(int i, int i1) {
+		return i * i1;
+	}
+
+	public void cos(int i, int i1) {
+		throw new UnsupportedOperationException("Trigonometry not supported!");
+
+	}
+
+	public void slowCalculation() {
+		try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException e) {
+			System.out.println("THIS OPERATION COMPLETED AFTER 3 SECONDS");
+
+		}
+
 	}
 
 }
